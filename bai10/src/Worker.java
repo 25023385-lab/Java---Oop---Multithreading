48b850dd429b021110dd25c9bf653475b5cc52f5
tuanminhngo01-cyber <tuanminhngo01@gmail.com>
2026-04-19 20:59:46 +0700

    bai 2

--- bai10/src/Worker.java
+++ bai10/src/Worker.java
@@ -1,4 +1,33 @@
 public class Worker implements Runnable {
-    Boolean running = true;
+    volatile Boolean running = true;
+    // tính hiện thị bộ nhớ
+    // chạy chương trình ở nhiều core khác nhau; bộ nhớ đệm của các core khi thay đổi giá trị thì 2 thằng sẽ không biết
+    // volatile bắt phải chạy trên RAM
+    @Override
+    public void run() {
+        while (running) {
+            System.out.println("Working");
+        }
+    }
+    public void stop() {
+        running = false;
+        System.out.println("Worker stopped");
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Worker worker = new Worker();
+        Thread t1 = new Thread(worker);
+        t1.start();
+        try{
+        Thread.sleep(1000);
+        worker.stop();
+        t1.join();
+        // ép luồng main phải chờ t1 chạy xong mới được chạy tiếp
 
+    }
+        catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
